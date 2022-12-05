1fcf99252aa07209f5822ba701c73f1018fb2388
Joao-Heitor <72704296+Joao-Heitor@users.noreply.github.com>
2022-12-04 23:43:28 -0300

    fix

--- frontEnd/src/main.js
+++ frontEnd/src/main.js
@@ -17,6 +17,7 @@
 
 window.addEventListener("load", getConsultas)
 
+//Listar Consultas
 const getConsultas = async () =>
 {
   try
@@ -36,51 +37,52 @@ const getConsultas = async () =>
   {
     console.log(e);
   }
+}
 
-  const getConsulta = async () =>
+//Consulta especifica por ID
+const getConsulta = async () =>
+{
+  const select   = document.querySelector("#consultas")
+  const results  = await fetch(`http://localhost:8080/consultas/${select.value}`);
+  const consulta = await results.json();
+
+  if (!!document.getElementById("temUmaDiv")) 
   {
-    const select   = document.querySelector("#consultas")
-    const results  = await fetch(`http://localhost:8080/consultas/${select.value}`);
-    const consulta = await results.json();
-
-    if (!!document.getElementById("temUmaDiv")) 
-    {
-      document.getElementById("temUmaDiv").remove();
-    }
-
-    const divGerada = document.createElement("div");
-    divGerada.id = "temUmaDiv";
-    document.body.append(divGerada);
-
-    const ul = document.createElement("ul");
-    divGerada.appendChild(ul);
-    ul.id = "temUmUl";
-
-    const liID           = document.createElement("li");
-    const liProcedimento = document.createElement("li");
-    const liOrcamento    = document.createElement("li");
-    const liObservacao   = document.createElement("li");
-    const liIdMedico     = document.createElement("li");
-    const liIdUsuario    = document.createElement("li");
-    const liData         = document.createElement("li");
-    const liStatus       = document.createElement("li");
-    
-    liID.innerText           = `ID: ${consulta.id}`;
-    liProcedimento.innerText = `Procedimentos: ${consulta.procedimento}`;
-    liOrcamento.innerText    = `Orçamento: ${consulta.orcamento}`;
-    liObservacao.innerText   = `Observação: ${consulta.observacao}`;
-    liIdMedico.innerText     = `ID Médico: ${consulta.medico}`;
-    liIdUsuario.innerText    = `ID Usuário: ${consulta.usuario}`;
-    liData.innerText         = `Data: ${consulta.data}`;
-    liStatus.innerText       = `Status: ${consulta.status}`;
-    
-    ul.appendChild(liID);
-    ul.appendChild(liProcedimento);
-    ul.appendChild(liObservacao);
-    ul.appendChild(liIdMedico);
-    ul.appendChild(liIdUsuario);
-    ul.appendChild(liData);
-    ul.appendChild(liStatus);
-
+    document.getElementById("temUmaDiv").remove();
   }
+
+  const divGerada = document.createElement("div");
+  divGerada.id = "temUmaDiv";
+  document.body.append(divGerada);
+
+  const ul = document.createElement("ul");
+  divGerada.appendChild(ul);
+  ul.id = "temUmUl";
+
+  const liID           = document.createElement("li");
+  const liProcedimento = document.createElement("li");
+  const liOrcamento    = document.createElement("li");
+  const liObservacao   = document.createElement("li");
+  const liIdMedico     = document.createElement("li");
+  const liIdUsuario    = document.createElement("li");
+  const liData         = document.createElement("li");
+  const liStatus       = document.createElement("li");
+  
+  liID.innerText           = `ID: ${consulta.id}`;
+  liProcedimento.innerText = `Procedimentos: ${consulta.procedimento}`;
+  liOrcamento.innerText    = `Orçamento: ${consulta.orcamento}`;
+  liObservacao.innerText   = `Observação: ${consulta.observacao}`;
+  liIdMedico.innerText     = `ID Médico: ${consulta.medico}`;
+  liIdUsuario.innerText    = `ID Usuário: ${consulta.usuario}`;
+  liData.innerText         = `Data: ${consulta.data}`;
+  liStatus.innerText       = `Status: ${consulta.status}`;
+  
+  ul.appendChild(liID);
+  ul.appendChild(liProcedimento);
+  ul.appendChild(liObservacao);
+  ul.appendChild(liIdMedico);
+  ul.appendChild(liIdUsuario);
+  ul.appendChild(liData);
+  ul.appendChild(liStatus);
+
 }
